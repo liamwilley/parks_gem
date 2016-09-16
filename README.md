b627c71bff3f1b8f288ed1bf7af3ef4503dfc86a
Liam Willey <wwilleymc@gmail.com>
2016-09-15 20:07:35 -0700

    unirest comands

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # ParksGem
-
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/parks_gem`. To experiment with that code, run `bin/console` for an interactive prompt.
+Time to display some park addresses
 
 TODO: Delete this and the text above, and describe your gem
 
--- lib/parks_gem.rb
+++ lib/parks_gem.rb
@@ -1,5 +1,16 @@
 require "parks_gem/version"
+require "unirest"
 
 module ParksGem
-  # Your code goes here...
+ class Parks
+
+  def initialize(park_hash)
+    @location = parks_hash[park][18][0]
+  end
+
+  def self.all? 
+    parks_array = Unirest.get("https://data.sfgov.org/api/views/z76i-7s65/rows.json?accessType=DOWNLOAD").body["data"]
+    create_parks(parks_array)
+  end
+
 end
